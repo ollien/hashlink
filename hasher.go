2df3ab448327192440ec227eceb5162ff58341aa
Nick Krichevsky <nick@ollien.com>
2019-08-07 23:34:44 -0400

    Shuffle things around and add comments

--- hasher.go
+++ hasher.go
@@ -12,6 +12,12 @@ import (
 
 var errNotFile = errors.New("not a file; will not hash")
 
+// hashFileDetails represents all info about a file needed to hash it
+type hashFileDetails struct {
+	path string
+	info os.FileInfo
+}
+
 // HashWalker represents something that can walk a tree and generate hashes
 type HashWalker interface {
 	// HashWalk takes a root path and returns a path of each file, along with its hash
@@ -19,28 +25,50 @@ type HashWalker interface {
 }
 
 // SerialHashWalker will hash all files one after the other
+// Implements HashWalker
 type SerialHashWalker struct {
 	constructor func() hash.Hash
 }
 
-// NewSerialHashWalker makes a new serial hasher with a constructor for the new hash algorithm
-// Implements HashWalker
+// makehashForFile generates a hash for the given file, provided that it is a regular file.
+// Returns errNotFile if the path is not a regular file
+func hashFile(h hash.Hash, details hashFileDetails) error {
+	if !details.info.Mode().IsRegular() {
+		return errNotFile
+	}
+
+	fileHandle, err := os.Open(details.path)
+	if err != nil {
+		return xerrors.Errorf("failed to open file (%s) to hash: %w", details.path, err)
+	}
+
+	_, err = io.Copy(h, fileHandle)
+	if err != nil {
+		return xerrors.Errorf("could not hash file (%s): %w", details.path, err)
+	}
+
+	return nil
+}
+
+// NewSerialHashWalker makes a new serial hasher with a constructor for a hash algorithm
 func NewSerialHashWalker(constructor func() hash.Hash) SerialHashWalker {
 	return SerialHashWalker{
 		constructor: constructor,
 	}
 }
 
-// HashWalk walks the given path and returns all
+// HashWalk walks the given path and returns hashes for all the files in the path
 func (walker *SerialHashWalker) HashWalk(root string) (map[string]hash.Hash, error) {
 	walkedMap := make(map[string]hash.Hash)
+	// Walk all of the files and collect hashes for them
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if path == root {
 			return nil
 		}
 
 		outHash := walker.constructor()
-		err = hashFile(outHash, path, info)
+		details := hashFileDetails{path: path, info: info}
+		err = hashFile(outHash, details)
 		if err == errNotFile {
 			// If we don't have a file, continue to the next one.
 			return nil
@@ -57,24 +85,4 @@ func (walker *SerialHashWalker) HashWalk(root string) (map[string]hash.Hash, err
 	}
 
 	return walkedMap, nil
-}
-
-// makehashForFile generates a hash for the given file, provided that it is a regular file.
-// Returns errNotFile if the path is not a regular file
-func hashFile(h hash.Hash, path string, info os.FileInfo) error {
-	if !info.Mode().IsRegular() {
-		return errNotFile
-	}
-
-	fileHandle, err := os.Open(path)
-	if err != nil {
-		return xerrors.Errorf("failed to open file (%s) to hash: %w", path, err)
-	}
-
-	_, err = io.Copy(h, fileHandle)
-	if err != nil {
-		return xerrors.Errorf("could not hash file (%s): %w", path, err)
-	}
-
-	return nil
 }
