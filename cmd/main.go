64a33874f0b54970d8002e330ec8a4486c3a1d69
Nick Krichevsky <nick@ollien.com>
2019-08-11 19:17:47 -0400

    Add flag parsing to main.go

--- cmd/main.go
+++ cmd/main.go
@@ -4,29 +4,52 @@ import (
 	"crypto/sha256"
 	"flag"
 	"fmt"
+	"hash"
 	"os"
 
 	"github.com/ollien/hashlink"
 	"github.com/ollien/xtrace"
 )
 
-const usage = "Usage: ./hashlink src_dir"
-
 func main() {
+	var numWorkers int
+	flag.Usage = func() {
+
+	}
+	flag.IntVar(&numWorkers, "j", 1, "specify a number of workers")
 	flag.Parse()
 	if flag.NArg() != 1 {
-		fmt.Println(usage)
+		Usage()
+		os.Exit(1)
+	}
+
+	if numWorkers <= 0 {
+		fmt.Fprintln(os.Stderr, "Invalid number of workers")
+		Usage()
 		os.Exit(1)
 	}
 
 	srcDir := flag.Arg(0)
-	hasher := hashlink.NewParallelWalkHasher(2, sha256.New)
+	var hasher hashlink.WalkHasher = hashlink.NewSerialWalkHasher(sha256.New)
+	if numWorkers >= 1 {
+		hasher = hashlink.NewParallelWalkHasher(numWorkers, sha256.New)
+	}
+
 	hashes, err := hasher.WalkAndHash(srcDir)
 	if err != nil {
 		xtrace.Trace(err)
 		os.Exit(1)
 	}
+	printResults(hashes)
+}
 
+// Usage specifies the usage for the cmd package
+func Usage() {
+	fmt.Fprintln(os.Stderr, "Usage: ./hashlink src_dir")
+	flag.PrintDefaults()
+}
+
+func printResults(hashes map[string]hash.Hash) {
 	buffer := make([]byte, 0)
 	for path, hash := range hashes {
 		sum := hash.Sum(buffer)
