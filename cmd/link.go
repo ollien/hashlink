7e2daf88e9b4f73bb2a9dea7d296851613d27e55
Nick Krichevsky <nick@ollien.com>
2019-08-17 15:14:41 -0400

    Break up main function

---
+++ cmd/link.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/ollien/hashlink"
+	"github.com/ollien/hashlink/multierror"
+	"golang.org/x/xerrors"
+)
+
+// linkFiles will hardlink all paths within filePaths to outDir in the same configuration as they were in srcDir
+// If the file does not exist in srcDir, no link will be created an error will be returned for that file.
+func linkFiles(files hashlink.FileMap, srcDir, outDir string) error {
+	errors := multierror.NewMultiError()
+	for _, identicalFiles := range files {
+		// TODO: This is a hack - we should implement a flip for FileMap
+		for _, file := range identicalFiles {
+			err := linkFile(file, srcDir, outDir)
+			if err != nil {
+				err = xerrors.Errorf("could not link file: %w", err)
+				errors.Append(err)
+			}
+		}
+	}
+
+	if errors.Len() > 0 {
+		return errors
+	}
+
+	return nil
+}
+
+// linkFile takes a file and will make a hardlink within the destination directory in the same configuration as it was
+// in srcDir. If the file does not exist in srcDir, no link will be created an error will be returned for that file.
+func linkFile(srcPath, srcDir, destDir string) error {
+	relSrcPath, err := filepath.Rel(srcDir, srcPath)
+	if err != nil {
+		return xerrors.Errorf("could not produce relative path for file linking - srcPath may not be contained in srcDir: %w", err)
+	}
+
+	outPath := path.Join(destDir, relSrcPath)
+	// TODO: correct permissions of directory to match original
+	err = ensureContainingDirsArePresent(outPath)
+	if err != nil {
+		return xerrors.Errorf("could not make directories for linking file (%s => %s): %w", srcPath, outPath, err)
+	}
+
+	return os.Link(srcPath, outPath)
+}
+
+// ensureContainignDirsArepResent ensures that the dirs needed for a file are fully present. Will make the directories
+// if needed. All file modes will be defaultFileMode, and should be corrected by the caller if anything else is desired.
+func ensureContainingDirsArePresent(filePath string) error {
+	dirComponent := path.Dir(filePath)
+	err := os.MkdirAll(dirComponent, defaultFileMode)
+	if err != nil {
+		return xerrors.Errorf("could not make directories for file (%s): %w", filePath, err)
+	}
+
+	return nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,51 +2,43 @@ package main
 
 import (
 	"crypto/sha256"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
-	"path"
-	"path/filepath"
 
 	"github.com/ollien/hashlink"
 	"github.com/ollien/hashlink/multierror"
 	"github.com/ollien/xtrace"
-	"golang.org/x/xerrors"
 )
 
 const defaultFileMode os.FileMode = 0755
 
+var (
+	errWrongNumberOfArguments = errors.New("wrong number of arguments")
+	errInvalidNumberOfWorkers = errors.New("invalid number of workers")
+)
+
+// cliArgs rpresents the arguments that can be passed to the entrypoint command
+type cliArgs struct {
+	numWorkers   int
+	srcDir       string
+	referenceDir string
+	outDir       string
+}
+
 func main() {
-	var numWorkers int
-	flag.Usage = Usage
-	flag.IntVar(&numWorkers, "j", 1, "specify a number of workers")
-	flag.Parse()
-	if flag.NArg() != 3 {
-		Usage()
-		os.Exit(1)
-	} else if numWorkers <= 0 {
-		fmt.Fprintln(os.Stderr, "Invalid number of workers")
-		Usage()
-		os.Exit(1)
-	}
+	args, err := setupAndValidateArgs()
+	hasher := getWalkHasher(args.numWorkers)
 
-	srcDir := flag.Arg(0)
-	referenceDir := flag.Arg(1)
-	outDir := flag.Arg(2)
-	// If we only have one worker, there's no point in spinning up a parallel hash walker.
-	var hasher hashlink.WalkHasher = hashlink.NewSerialWalkHasher(sha256.New)
-	if numWorkers > 1 {
-		hasher = hashlink.NewParallelWalkHasher(numWorkers, sha256.New)
-	}
-
-	srcHashes, err := hasher.WalkAndHash(srcDir)
+	srcHashes, err := hasher.WalkAndHash(args.srcDir)
 	if err != nil {
 		// Some hash walkers make use of MultiErrors, so we should try to unpack those first if we can.
 		handleMultiError(err)
 		os.Exit(1)
 	}
 
-	referenceHashes, err := hasher.WalkAndHash(referenceDir)
+	referenceHashes, err := hasher.WalkAndHash(args.referenceDir)
 	if err != nil {
 		handleMultiError(err)
 		os.Exit(1)
@@ -62,7 +54,7 @@ func main() {
 		fmt.Print("\n")
 	}
 
-	err = linkFiles(identicalFiles, srcDir, outDir)
+	err = linkFiles(identicalFiles, args.srcDir, args.outDir)
 	if err != nil {
 		handleMultiError(err)
 		os.Exit(1)
@@ -77,10 +69,34 @@ func Usage() {
 	flag.PrintDefaults()
 }
 
-func printResults(fileMap hashlink.FileMap) {
-	for path, matchingFiles := range fileMap {
-		fmt.Printf("%s => %v\n", path, matchingFiles)
+func setupAndValidateArgs() (cliArgs, error) {
+	args := cliArgs{}
+	flag.Usage = Usage
+	flag.IntVar(&args.numWorkers, "j", 1, "specify a number of workers")
+	flag.Parse()
+	if flag.NArg() != 3 {
+		return cliArgs{}, errWrongNumberOfArguments
+	} else if args.numWorkers <= 0 {
+		return cliArgs{}, errInvalidNumberOfWorkers
 	}
+
+	args.srcDir = flag.Arg(0)
+	args.referenceDir = flag.Arg(1)
+	args.outDir = flag.Arg(2)
+	// TODO: Validate these are directories and existant
+
+	return args, nil
+}
+
+func handleArgsError(err error) {
+	if err == errInvalidNumberOfWorkers {
+		fmt.Fprintf(os.Stderr, "Invalid number of workers. Must be >= 1")
+	} else if err != errWrongNumberOfArguments {
+		// If we have errWrongNumberOfArguments, we don't need to do any special handling other than the usage string.
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	Usage()
 }
 
 func handleMultiError(err error) {
@@ -97,54 +113,12 @@ func handleMultiError(err error) {
 	}
 }
 
-// linkFiles will hardlink all paths within filePaths to outDir in the same configuration as they were in srcDir
-// If the file does not exist in srcDir, no link will be created an error will be returned for that file.
-func linkFiles(files hashlink.FileMap, srcDir, outDir string) error {
-	errors := multierror.NewMultiError()
-	for _, identicalFiles := range files {
-		// TODO: This is a hack - we should implement a flip for FileMap
-		for _, file := range identicalFiles {
-			err := linkFile(file, srcDir, outDir)
-			if err != nil {
-				err = xerrors.Errorf("could not link file: %w", err)
-				errors.Append(err)
-			}
-		}
+// getWalkHasher gets the approrpiate WalkHasher based on the number of workers
+func getWalkHasher(numWorkers int) hashlink.WalkHasher {
+	// If we only have one worker, there's no point in spinning up a parallel hash walker.
+	if numWorkers > 1 {
+		return hashlink.NewParallelWalkHasher(numWorkers, sha256.New)
 	}
 
-	if errors.Len() > 0 {
-		return errors
-	}
-
-	return nil
-}
-
-// linkFile takes a file and will make a hardlink within the destination directory in the same configuration as it was
-// in srcDir. If the file does not exist in srcDir, no link will be created an error will be returned for that file.
-func linkFile(srcPath, srcDir, destDir string) error {
-	relSrcPath, err := filepath.Rel(srcDir, srcPath)
-	if err != nil {
-		return xerrors.Errorf("could not produce relative path for file linking - srcPath may not be contained in srcDir: %w", err)
-	}
-
-	outPath := path.Join(destDir, relSrcPath)
-	// TODO: correct permissions of directory to match original
-	err = ensureContainingDirsArePresent(outPath)
-	if err != nil {
-		return xerrors.Errorf("could not make directories for linking file (%s => %s): %w", srcPath, outPath, err)
-	}
-
-	return os.Link(srcPath, outPath)
-}
-
-// ensureContainignDirsArepResent ensures that the dirs needed for a file are fully present. Will make the directories
-// if needed. All file modes will be defaultFileMode, and should be corrected by the caller if anything else is desired.
-func ensureContainingDirsArePresent(filePath string) error {
-	dirComponent := path.Dir(filePath)
-	err := os.MkdirAll(dirComponent, defaultFileMode)
-	if err != nil {
-		return xerrors.Errorf("could not make directories for file (%s): %w", filePath, err)
-	}
-
-	return nil
+	return hashlink.NewSerialWalkHasher(sha256.New)
 }
