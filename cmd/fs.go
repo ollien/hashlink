786370b88de4c3212bb4938904a56fb6e2e5c761
Nick Krichevsky <nick@ollien.com>
2019-08-17 17:04:18 -0400

    Add dry run ability

--- cmd/fs.go
+++ cmd/fs.go
@@ -10,12 +10,18 @@ import (
 	"golang.org/x/xerrors"
 )
 
-// linkFiles will hardlink all paths within filePaths to outDir in the same configuration as they were in srcDir
-// If the file does not exist in srcDir, no link will be created an error will be returned for that file.
-func linkFiles(files hashlink.FileMap, srcDir, outDir string) error {
+const defaultFileMode os.FileMode = 0755
+
+type connectFunction = func(src, dst string) error
+
+// connectFiles performs the given function op on all provided files, in order to form a connection between them, such
+// as copying or hard linking. All of the files in the keys of fileMap must be contained within srcDir, and outDir will
+// follow the same structur as srcDir. If the file does not exist in srcDir, no link will be created an error will be
+// returned for that file.
+func connectFiles(files hashlink.FileMap, srcDir, outDir string, op connectFunction) error {
 	errors := multierror.NewMultiError()
 	for file := range files {
-		err := linkFile(file, srcDir, outDir)
+		err := connectFile(file, srcDir, outDir, op)
 		if err != nil {
 			err = xerrors.Errorf("could not link file: %w", err)
 			errors.Append(err)
@@ -29,22 +35,23 @@ func linkFiles(files hashlink.FileMap, srcDir, outDir string) error {
 	return nil
 }
 
-// linkFile takes a file and will make a hardlink within the destination directory in the same configuration as it was
-// in srcDir. If the file does not exist in srcDir, no link will be created an error will be returned for that file.
-func linkFile(srcPath, srcDir, destDir string) error {
+// connectFiles performs the given function op on the provided file, in order to form a connection between them, such
+// as copying or hard linking. outDir will follow the same structur as srcDir. If the file does not exist in srcDir,
+// no link will be created an error will be  returned for that file.
+func connectFile(srcPath, srcDir, outDir string, op connectFunction) error {
 	relSrcPath, err := filepath.Rel(srcDir, srcPath)
 	if err != nil {
 		return xerrors.Errorf("could not produce relative path for file linking - srcPath may not be contained in srcDir: %w", err)
 	}
 
-	outPath := path.Join(destDir, relSrcPath)
+	outPath := path.Join(outDir, relSrcPath)
 	// TODO: correct permissions of directory to match original
 	err = ensureContainingDirsArePresent(outPath)
 	if err != nil {
 		return xerrors.Errorf("could not make directories for linking file (%s => %s): %w", srcPath, outPath, err)
 	}
 
-	return os.Link(srcPath, outPath)
+	return op(srcPath, outPath)
 }
 
 // ensureContainignDirsArepResent ensures that the dirs needed for a file are fully present. Will make the directories
--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha256"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -13,8 +14,6 @@ import (
 	"golang.org/x/xerrors"
 )
 
-const defaultFileMode os.FileMode = 0755
-
 var (
 	errWrongNumberOfArguments = errors.New("wrong number of arguments")
 	errInvalidNumberOfWorkers = errors.New("invalid number of workers")
@@ -22,6 +21,7 @@ var (
 
 // cliArgs rpresents the arguments that can be passed to the entrypoint command
 type cliArgs struct {
+	dryRun       bool
 	numWorkers   int
 	srcDir       string
 	referenceDir string
@@ -31,7 +31,7 @@ type cliArgs struct {
 func main() {
 	args, err := setupAndValidateArgs()
 	if err != nil {
-		handleMultiError(err)
+		handleError(err)
 		os.Exit(1)
 	}
 
@@ -40,13 +40,13 @@ func main() {
 	srcHashes, err := hasher.WalkAndHash(args.srcDir)
 	if err != nil {
 		// Some hash walkers make use of MultiErrors, so we should try to unpack those first if we can.
-		handleMultiError(err)
+		handleError(err)
 		os.Exit(1)
 	}
 
 	referenceHashes, err := hasher.WalkAndHash(args.referenceDir)
 	if err != nil {
-		handleMultiError(err)
+		handleError(err)
 		os.Exit(1)
 	}
 
@@ -57,23 +57,35 @@ func main() {
 	missingFiles := hashlink.GetUnmappedFiles(referenceHashes, flippedIdenticalFiles)
 	fmt.Print("Done scanning.")
 	if len(missingFiles) > 0 {
-		fmt.Printf("The following files will not be processed.\n%v\n", missingFiles)
+		missingFilesOutput, err := makeJSONEncodedStringSlice(missingFiles)
+		if err != nil {
+			err = xerrors.Errorf("could not generate missing file output: %w", err)
+			handleError(err)
+		}
+
+		fmt.Printf("The following files will not be processed.\n%v\n", missingFilesOutput)
 	} else {
 		fmt.Print("\n")
 	}
 
-	err = linkFiles(identicalFiles, args.srcDir, args.outDir)
+	op := getConnectFunction(args.dryRun, os.Link)
+	err = connectFiles(identicalFiles, args.srcDir, args.outDir, op)
 	if err != nil {
-		handleMultiError(err)
+		handleError(err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Done linking. Enjoy your files :)")
+	output := "Done linking. Enjoy your files :)"
+	if args.dryRun {
+		output = getDryRunOutput(identicalFiles)
+	}
+
+	fmt.Println(output)
 }
 
 // Usage specifies the usage for the cmd package
 func Usage() {
-	fmt.Fprintln(os.Stderr, "Usage: ./hashlink [-j n] src_dir reference_dir out_dir")
+	fmt.Fprintln(os.Stderr, "Usage: ./hashlink [-j n] [-n] src_dir reference_dir out_dir")
 	flag.PrintDefaults()
 }
 
@@ -81,6 +93,7 @@ func setupAndValidateArgs() (cliArgs, error) {
 	args := cliArgs{}
 	flag.Usage = Usage
 	flag.IntVar(&args.numWorkers, "j", 1, "specify a number of workers")
+	flag.BoolVar(&args.dryRun, "n", false, "do not link any files, but print out what files would have been linked")
 	flag.Parse()
 	if flag.NArg() != 3 {
 		return cliArgs{}, errWrongNumberOfArguments
@@ -107,7 +120,7 @@ func handleArgsError(err error) {
 	Usage()
 }
 
-func handleMultiError(err error) {
+func handleError(err error) {
 	multiErr, isMulti := err.(*multierror.MultiError)
 	if isMulti {
 		for i, singleError := range multiErr.Errors() {
@@ -129,6 +142,17 @@ func getWalkHasher(numWorkers int) hashlink.WalkHasher {
 	}
 
 	return hashlink.NewSerialWalkHasher(sha256.New)
+}
+
+// getConnectFunction gives a nop function if dryRun is try, the fallback otherwise.
+func getConnectFunction(dryRun bool, fallback connectFunction) connectFunction {
+	if dryRun {
+		return func(src, dst string) error {
+			return nil
+		}
+	}
+
+	return fallback
 }
 
 // assertDirsExist will return true if all of the paths in the values of the map exist.
@@ -154,4 +178,29 @@ func assertDirsExist(dirs ...string) error {
 	}
 
 	return nil
+}
+
+// getDryRunOutput gets the output for the termination of the program when the dryRun flag is provided.
+func getDryRunOutput(identicalFiles hashlink.FileMap) string {
+	srcFiles := make([]string, len(identicalFiles))
+	i := 0
+	for file := range identicalFiles {
+		srcFiles[i] = file
+		i++
+	}
+
+	out, err := makeJSONEncodedStringSlice(srcFiles)
+	if err != nil {
+		handleError(err)
+		os.Exit(1)
+	}
+
+	return out
+}
+
+// makeJSONEncodedStringSlice makes a JSON formatted string of the given string slice
+func makeJSONEncodedStringSlice(s []string) (string, error) {
+	out, err := json.MarshalIndent(s, "", "\t")
+
+	return string(out), err
 }
